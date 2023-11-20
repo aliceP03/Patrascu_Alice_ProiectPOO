3f31d413b9e729b3ba4f78225ed50bed18f6fea2
aliceP03 <148048197+aliceP03@users.noreply.github.com>
2023-11-20 13:16:53 +0200

    Update FZ1.cpp

--- FZ1.cpp
+++ FZ1.cpp
@@ -155,7 +155,7 @@ public:
 			p.areAsigurare = false;
 		return input_p;
 	}
-
+	friend class Doctor;
 };
 
 int Pacient::nrGenerator = 0;
@@ -348,17 +348,23 @@ public:
 	}
 
 	friend istream& operator>>(istream& input_dm, DepartamentMedical& dm) {
+		if (dm.numeAngajati != NULL) {
+			delete[] dm.numeAngajati;
+			dm.numeAngajati = NULL;
+		}
+		if (dm.salarii != NULL) {
+			delete[] dm.salarii;
+			dm.salarii = NULL; 
+		}
 		cout << "Numar angajati: ";
 		input_dm >> dm.nrAngajati;
-		if (dm.numeAngajati) {
-			delete[]dm.numeAngajati;
-		}
-		if (dm.salarii) {
-			delete[]dm.salarii;
+		if (dm.nrAngajati <= 0) {
+			cerr << "Numarul de angajati trebuie sa fie un numar pozitiv nenul.";
+			return input_dm;
 		}
 		dm.numeAngajati = new string[dm.nrAngajati];
 		dm.salarii = new float[dm.nrAngajati];
-		cout << " Introduceti numele angajatilor:";
+		cout << " Introduceti numele angajatilor:\n";
 		for (int i = 0; i < dm.nrAngajati; i++){
 			input_dm >>dm. numeAngajati[i];
 			cout << "Angajatul cu numele " << dm.numeAngajati[i] << " are salariul de (lei): ";
@@ -561,17 +567,115 @@ public:
 };
 int EchipamentMedical::nrGeneratorEchipament = 0; 
 
+class Doctor {
+	string nume;
+	int nrPacienti;
+	Pacient* pacienti;
+
+public:
+
+	string getNume() {
+		return this->nume;
+	}
+
+	int getNrPacienti() {
+		return this->nrPacienti;
+	}
+
+	Pacient getPacienti(int index) {
+		if (index > 0 && index < this->nrPacienti)
+			return pacienti[index];
+	}
+
+	void setNume(string nume) {
+		if (nume.length()>0)
+		this->nume = nume;
+	}
+
+	void setPacienti(int nr, Pacient* pacienti) {
+		if (nr > 0) {
+			this->nrPacienti = nr;
+			if (this->pacienti)
+				delete[]pacienti;
+			this->pacienti = new Pacient[nr];
+			for (int i = 0; i < nr; i++)
+				this->pacienti[i] = pacienti[i];
+		}
+	}
+
+	Doctor(string nume, int nrPacienti, Pacient *pacienti) {
+		this->nume = nume;
+		this->nrPacienti = nrPacienti;
+		this->pacienti = new Pacient[this->nrPacienti];
+		for (int i = 0; i < this->nrPacienti; i++)
+			this->pacienti[i] = pacienti[i];
+	}
+
+	Doctor(const Doctor& d) {
+		this->nume = d.nume;
+		this->nrPacienti = d.nrPacienti;
+		this->pacienti = new Pacient[nrPacienti];
+		for (int i = 0; i < this->nrPacienti; i++)
+			this->pacienti[i] = d.pacienti[i];
+	}
+
+
+	Doctor operator=(const Doctor& d) {
+		if (this != &d) {
+			this->nume = d.nume;
+			this->nrPacienti = d.nrPacienti;
+			if (this->pacienti)
+				delete[] pacienti;
+			this->pacienti = new Pacient[nrPacienti];
+			for (int i = 0; i < this->nrPacienti; i++)
+				this->pacienti[i] = d.pacienti[i];
+		}
+		return *this;
+	}
+
+	~Doctor() {
+		if (this->pacienti)
+			delete[] pacienti;
+	}
+
+	bool operator>(Doctor d) {
+		return this->nrPacienti > d.nrPacienti;
+	}
+
+	friend ostream& operator<< (ostream& out,const Doctor &d) {
+		out << "Doctorul " << d.nume << " are " << d.nrPacienti << " pacienti: ";
+		for (int i = 0; i < d.nrPacienti; i++)
+			out << d.pacienti[i]<< endl;
+		out << endl;
+		return out;
+	}
+
+	friend istream& operator>> (istream& in, Doctor& d) {
+		cout << "Introduceti datele doctorului. Nume: ";
+		in >> d.nume;
+		cout << "Numarul actual de pacienti in grija:";
+		in >> d.nrPacienti;
+		d.pacienti = new Pacient[d.nrPacienti];
+		for (int i = 0; i < d.nrPacienti; i++) {
+			cout << "Datele pacientului " << i + 1 << " sunt :  ";
+			in >> d.pacienti[i];
+		}
+
+		return in;
+	}
+	
+};
 
 
 
 void main() {
-	//clasa 1
+	//clasa pacient(1)
 	Pacient pacient1;
 	pacient1.AfisarePacient();
 
 	char* nume = new char[strlen("Ionescu Ion") + 1];
 	strcpy_s(nume, strlen("Ionescu Ion") + 1, "Ionescu Ion");
-	Pacient pacient2(nume,25,false);
+	Pacient pacient2(nume, 25, false);
 	pacient2.AfisarePacient();
 	modificareAsigurare(pacient2);
 	pacient2.AfisarePacient(); //apelare functie prietena cu clasa pacient 
@@ -580,15 +684,15 @@ void main() {
 	Pacient pacient3(50, true);
 	pacient3.AfisarePacient();
 
-	cout<< "Numarul de pacienti consultati pana in acest moment este de "<< Pacient::getNrGenerator()<<"." << endl;
+	cout << "Numarul de pacienti consultati pana in acest moment este de " << Pacient::getNrGenerator() << "." << endl;
 
 
 
 	Pacient pacient4(pacient2);//copiere prin constructorul de copiere 
-	cout<<"Pacientul cu numarul " << pacient4.getNrPacient()+1 <<" are numele de "<< pacient4.getNume() << " , varsta de "<< pacient4.getVarsta() 
-		<<" (de) ani  si costurile " 
-		<<( pacient4.getAreAsigurare()? "vor fi acoperite de asigurarea medicala.": "nu vor fi acoperite de asigurarea medicala.") << endl;
-	char* nume4 = new char[strlen("Palade Maria")+1];
+	cout << "Pacientul cu numarul " << pacient4.getNrPacient() + 1 << " are numele de " << pacient4.getNume() << " , varsta de " << pacient4.getVarsta()
+		<< " (de) ani  si costurile "
+		<< (pacient4.getAreAsigurare() ? "vor fi acoperite de asigurarea medicala." : "nu vor fi acoperite de asigurarea medicala.") << endl;
+	char* nume4 = new char[strlen("Palade Maria") + 1];
 	strcpy_s(nume4, strlen("Palade Maria") + 1, "Palade Maria");
 	pacient4.setNume(nume4);
 	pacient4.setVarsta(15);
@@ -599,9 +703,17 @@ void main() {
 
 	Pacient pacient5;
 	pacient5 = pacient2;//copiere prin operatorul =
-	pacient5.AfisarePacient(); 
+	pacient5.AfisarePacient();
 
+	//clasa doctor(4)
+	Pacient* p = new Pacient[2]{ pacient1, pacient2 };
+	Doctor dr1("Florescu", 2, p);
+	cout << dr1;
+	Doctor dr2 = dr1;
+	cin >> dr2;
+	cout << dr2;
 
+	//clasa pacient(1)
 	Pacient pacient6;
 	cin >> pacient6;
 	cout << pacient6;
@@ -610,10 +722,10 @@ void main() {
 	cout << pacient6;
 	Pacient pacient7;
 	cin >> pacient7;
-	cout <<(pacient6 > pacient7 ? "Primul pacient este mai in varsta. " : "Al doilea pacient este mai in varsta.")<<endl;
-	cout << (pacient7 == pacient6 ? "Pacientii au aceeasi varsta." : "Pacientii nu au aceeasi varsta.")<<endl;
+	cout << (pacient6 > pacient7 ? "Primul pacient este mai in varsta. " : "Al doilea pacient este mai in varsta.") << endl;
+	cout << (pacient7 == pacient6 ? "Pacientii au aceeasi varsta." : "Pacientii nu au aceeasi varsta.") << endl;
 
-	int Nr=0;
+	int Nr = 0;
 	cout << "Introduceti numarul pacientilor: ";
 	cin >> Nr;
 	Pacient* pacienti;
@@ -621,12 +733,12 @@ void main() {
 	for (int i = 0; i < Nr; i++) {
 		cin >> pacienti[i];
 	}
-	for(int i=0; i<Nr; i++)
+	for (int i = 0; i < Nr; i++)
 		cout << pacienti[i];
 
 
 
-	//clasa 2
+	//clasa departament medical(2)
 	DepartamentMedical dm1;
 	dm1.afisare();
 
@@ -636,17 +748,17 @@ void main() {
 	float* salarii2 = new float[2];
 	salarii2[0] = DepartamentMedical::getSalariuMinim() + 2000.00f;
 	salarii2[1] = DepartamentMedical::getSalariuMinim() + 650.00f;
-	DepartamentMedical dm2(2,angajati2,salarii2);
+	DepartamentMedical dm2(2, angajati2, salarii2);
 	dm2.afisare();
 
 	DepartamentMedical::MarireSalariu(20);
-	
+
 	string* angajati = new string[2];
 	angajati[0] = "Marinescu";
 	angajati[1] = "Stoica";
 	float* salarii = new float[2];
-	salarii[0] =  2000;
-	salarii[1] =  6650;
+	salarii[0] = 2000;
+	salarii[1] = 6650;
 	DepartamentMedical dm3("Pediatrie", 2, angajati, salarii);
 	dm3.afisare();
 	adaugareAngajat(dm3, "Palade", 3500); //apelare functie friend cu clasa departament medical
@@ -657,9 +769,9 @@ void main() {
 	DepartamentMedical dm4(dm1);
 	DepartamentMedical dm5;
 	cout << "Departamentul de " << dm4.getNume() << " are un numar de " << dm4.getNrAngajati() << " angajati cu salariile urmatoare: " << endl;
-		for (int i = 0; i < dm4.getNrAngajati(); i++) {
-			cout << "       " << i + 1 << ". " << dm4.getNumeAngajati(i) << " are salariul de " << dm4.getSalarii(i) << " lei." << endl;
-		}
+	for (int i = 0; i < dm4.getNrAngajati(); i++) {
+		cout << "       " << i + 1 << ". " << dm4.getNumeAngajati(i) << " are salariul de " << dm4.getSalarii(i) << " lei." << endl;
+	}
 	cout << endl;
 	dm5 = dm1;
 	dm5.afisare();
@@ -671,32 +783,32 @@ void main() {
 		cout << "       " << i + 1 << ". " << dm4.getNumeAngajati(i) << " are salariul de " << dm4.getSalarii(i) << " lei." << endl;
 	}
 	cout << endl;
-	cout << "Salariul minim din spital este de " << DepartamentMedical::getSalariuMinim()<<" lei." << endl;
+	cout << "Salariul minim din spital este de " << DepartamentMedical::getSalariuMinim() << " lei." << endl;
 	cout << endl;
 
 
 
-	cout << "Bugetul pentru salarii in sectia de " << dm3.getNume() << " este de " << dm3()<< " lei."<<endl;
-	cout<<dm3;
+	cout << "Bugetul pentru salarii in sectia de " << dm3.getNume() << " este de " << dm3() << " lei." << endl;
+	cout << dm3;
 	cin >> dm3;
 	dm3[0] = "Amariei";
-	cout << "Noul angajat al departamentului "<< dm3.getNume()<< " este "<<dm3[0]<<"." << endl;
+	cout << "Noul angajat al departamentului " << dm3.getNume() << " este " << dm3[0] << "." << endl;
 	dm3 += dm2;
 	cout << dm3;
 	cout << "Introduceti numarul departamentelor: ";
-	int nrr=0;
-	cin >> nrr;
-	DepartamentMedical* dm = new DepartamentMedical[nrr];
-	for (int i = 0; i < nrr; i++) {
+	int NR = 0;
+	cin >> NR;
+	DepartamentMedical* dm = new DepartamentMedical[NR];
+	for (int i = 0; i < NR; i++) {
 		cin >> dm[i];
 	}
-	for (int i = 0; i < nrr; i++) {
+	for (int i = 0; i < NR; i++) {
 		cout << dm[i];
 	}
 	int nrAngajatiDep1 = (int)dm1;
-	cout <<endl<< "Departamentul 1 are " <<nrAngajatiDep1<< " angajati";
-	
-	//clasa 3
+	cout << endl << "Departamentul 1 are " << nrAngajatiDep1 << " angajati";
+
+	//clasa echipament medical (3)
 	EchipamentMedical em1;
 	em1.afisareEchipament();
 
@@ -714,7 +826,7 @@ void main() {
 	EchipamentMedical em5;
 	em5 = em3; //copiere prin operatorul =
 	cout << "Echipamentul cu numarul de inregistrare " << em4.getNrEchipament() + 1 << " este un " << em4.getNume() << " cu durata medie de functionare de "
-		<< em4.getDurataMedieFunctionare() << " ani si " <<( em4.getEstePortabil() ? "este portabil." : "nu este portabil.");
+		<< em4.getDurataMedieFunctionare() << " ani si " << (em4.getEstePortabil() ? "este portabil." : "nu este portabil.");
 	cout << endl;
 	em4.setDuarataMedieFunctionare(20);
 	em4.setEstePortabil(true);
@@ -727,46 +839,48 @@ void main() {
 	em5.afisareEchipament();
 	cout << endl;
 	cout << "Numarul total de echipamente existente in spital este de " << EchipamentMedical::getNumarTotal() << "." << endl;
-	
+
 
 	cin >> em5;
 	cout << em5--;
 	cout << --em5;
 	cout << !em5;
-	cout << (em5 != em4 ? "Au durata medie de functionare diferite." : "Au aceeasi durata medie de functionare.")<<endl;
-	
-	
+	cout << (em5 != em4 ? "Au durata medie de functionare diferite." : "Au aceeasi durata medie de functionare.") << endl;
+
+
 	cout << "Introduceti numarul echipamentelor: ";
-	int nrrr=0;
-	cin >> nrrr;
-	EchipamentMedical* Em = new EchipamentMedical[nrrr];
-	for (int i = 0; i < nrrr; i++)
+	int numar = 0;
+	cin >> numar;
+	EchipamentMedical* Em = new EchipamentMedical[numar];
+	for (int i = 0; i < numar; i++)
 		cin >> Em[i];
-	for (int i = 0; i < nrrr; i++)
-		cout<< Em[i]<<endl;
+	for (int i = 0; i < numar; i++)
+		cout << Em[i] << endl;
 
 	int nrLinii;
-	int nrColoane ;
+	int nrColoane;
 	cout << "Cate tipuri de echipamente doriti sa introduceti? ";
 	cin >> nrColoane;
 	cout << "Cate echipamente din fiecare tip aveti?";
 	cin >> nrLinii;
-	EchipamentMedical** emm = new EchipamentMedical* [nrLinii];
+	EchipamentMedical** echipamente = new EchipamentMedical * [nrLinii];
 	for (int i = 0; i < nrLinii; i++)
 	{
-		emm[i] = new EchipamentMedical[nrColoane];
+		echipamente[i] = new EchipamentMedical[nrColoane];
 	}
 
 	for (int j = 0; j < nrColoane; j++)
 		for (int i = 0; i < nrLinii; i++) {
-			cout << "Echipamentul de tipul " << j + 1 <<  " cu numarul " << i + 1 << ":  ";
-			cin >> emm[i][j];
+			cout << "Echipamentul de tipul " << j + 1 << " cu numarul " << i + 1 << ":  ";
+			cin >> echipamente[i][j];
 		}
 	for (int i = 0; i < nrLinii; i++)
 		for (int j = 0; j < nrColoane; j++)
-			cout << emm[i][j]<<endl;
-	
+			cout << echipamente[i][j] << endl;
+
+
+
 	delete[]salarii; delete[]salarii2; delete[] angajati; delete[] angajati2;
-	delete[]pacienti; delete[]dm; delete[] em;
-	
+	delete[]pacienti; delete[]dm; delete[] em; delete[]p;
+
 }
